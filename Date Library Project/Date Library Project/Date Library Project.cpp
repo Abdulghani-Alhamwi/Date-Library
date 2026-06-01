05be8323b9efa5a7215c37409f4262841e072ab9
AbdulghaniAlhamwi <AbdulghaniAlhamwi3@gmail.com>
2026-06-01 19:55:25 +0300

    Added Project Files

---
+++ Date Library Project/Date Library Project/Date Library Project.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include "clsDate.h"
+
+using namespace std;
+
+int main()
+
+{
+    clsDate Date1;
+    Date1.Print();
+
+    clsDate Date2("31/1/2022");
+    Date2.Print();
+
+    clsDate Date3(20, 12, 2022);
+    Date3.Print();
+
+    clsDate Date4(250, 2022);
+    Date4.Print();
+
+    Date1.IncreaseDateByOneMonth();
+    Date1.Print();
+
+    //Date3.PrintYearCalendar();
+    //cout << Date2.IsValid() << endl;
+
+    /*cout << "My Age InDays:" <<
+        clsDate::CalculateMyAgeInDays(  clsDate(6, 11, 1995) );*/
+
+    cout << clsDate::CalculateMyAgeInDays(clsDate(30, 7, 2000));
+
+    system("pause>0");
+    return 0;
+};
+
